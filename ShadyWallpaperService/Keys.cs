c24af6551b7e6fc601fce758b9ea97026d4a5beb
DariusL <d.lapunas@gmail.com>
2014-11-06 18:01:57 +0200

    Stuff

---
+++ ShadyWallpaperService/Keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShadyWallpaperService
+{
+    public static class Keys
+    {
+        public const string User = "reader-mongo";
+        public const string Pass = "tangerinesfromhell";
+    }
+}
